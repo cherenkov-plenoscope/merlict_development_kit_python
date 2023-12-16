31c164556085dbe1b199090e19960107f713d0ea
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-12-16 16:24:02 +0100

    nameing

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ import os
 with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open(os.path.join("merlict_development_kit_python", "version.py")) as f:
+with open(
+    os.path.join(
+        "merlict_development_kit_python_cherenkov-plenoscope-project",
+        "version.py",
+    )
+) as f:
     txt = f.read()
     last_line = txt.splitlines()[-1]
     version_string = last_line.split()[-1]
@@ -14,7 +19,10 @@ with open(os.path.join("merlict_development_kit_python", "version.py")) as f:
 setuptools.setup(
     name="merlict_development_kit_python",
     version=version,
-    description=("This is merlict_development_kit_python."),
+    description=(
+        "A python package to ease the call of executables in "
+        "the merlict_development_kit."
+    ),
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/cherenkov-plenoscope/merlict_development_kit_python",
@@ -26,6 +34,7 @@ setuptools.setup(
     package_data={"merlict_development_kit_python": []},
     install_requires=[
         "json_utils_sebastian-achim-mueller",
+        "photon_spectra_cherenkov-plenoscope-project",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
